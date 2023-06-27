ac63f0e8cffd22233ac1dc045fb3d5c310a282a0
orlok547 <84822034+orlok547@users.noreply.github.com>
2023-06-27 04:20:21 -0300

    Added python file

---
+++ password-generator.py
@@ -0,0 +1,29 @@
+import random
+import string
+
+def generate_password(length):
+	characters = string.ascii_letters + string.digits + string.punctuation
+	forbidden_characters = ["\"", "'", "`", "´", "[", "]", "{", "}", "(", ")", ",", ";", ".", ":", "~", "¬", "^", "¨", "ç", "ª", "º"]
+	valid_characters = [char for char in characters if char not in forbidden_characters]
+	password = ''.join(random.choice(valid_characters) for _ in range(length))
+	return password
+
+# Specify the desired password length
+# password_length = 12
+
+""" password_length = int(input("Specify a password length: "))
+while password_length < 8:
+	password_length = int(input("Specify a password length: ")) """
+
+while True:
+	password_length = int(input("Specify a password length (min 8 - max 25): "))
+	if password_length < 8:
+		print("Number must be greater than 8.")
+	elif password_length > 25:
+		print("Number can't be greater than 25.")
+	else:
+		break
+
+# Generate the password
+password = generate_password(password_length)
+print(password)
